d590bff3ed79d7902c8107a5b99530f06f969198
Sebastian Schedegger <sebastian.scheidegger@sbb.ch>
2026-05-06 16:05:42 +0200

    ODISANDOSAINDS

---
+++ SAODS/html/_static/project_map.js
@@ -0,0 +1,27 @@
+// map of the project for the stable_uri forwarder
+const projectMap = {
+ "input/0.html": [
+
+
+  {"_LINK":"None-Administration-Specification" },
+
+  {"UID":"EL-161","_LINK":"EL-161" },
+
+  {"UID":"EL-187","_LINK":"EL-187" },
+
+  {"_LINK":"c8833970df76436c8fe3e7e93b6e706e" },
+
+  {"UID":"EL-186","_LINK":"EL-186" },
+
+  {"_LINK":"c56d489bfb094b819f8a3392f7759dae" },
+
+  {"UID":"EL-162","_LINK":"EL-162" },
+
+  {"UID":"EL-163","_LINK":"EL-163" },
+
+  {"UID":"EL-165","_LINK":"EL-165" },
+
+  {"UID":"EL-166","_LINK":"EL-166" },
+
+  {"UID":"EL-164","_LINK":"EL-164" }, ],
+};
