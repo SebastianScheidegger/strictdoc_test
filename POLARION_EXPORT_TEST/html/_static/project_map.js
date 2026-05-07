b921ecc6899cd18c939e79a7b311b9dc03ea12e3
polarion <polarion@906b2031d197>
2026-05-07 12:32:25 +0000

    Hello from polarion

---
+++ POLARION_EXPORT_TEST/html/_static/project_map.js
@@ -0,0 +1,27 @@
+// map of the project for the stable_uri forwarder
+const projectMap = {
+ "input/0.html": [
+
+
+  {"_LINK":"None-Administration-Specification" },
+
+  {"UID":"EL-161","_LINK":"EL-161" },
+
+  {"UID":"EL-187","_LINK":"EL-187" },
+
+  {"_LINK":"2f7ee5968ada4a0b8ec187028bd70d3e" },
+
+  {"UID":"EL-186","_LINK":"EL-186" },
+
+  {"_LINK":"f4bf1d77cdeb48c689d939997465dbf7" },
+
+  {"UID":"EL-162","_LINK":"EL-162" },
+
+  {"UID":"EL-163","_LINK":"EL-163" },
+
+  {"UID":"EL-165","_LINK":"EL-165" },
+
+  {"UID":"EL-166","_LINK":"EL-166" },
+
+  {"UID":"EL-164","_LINK":"EL-164" }, ],
+};
