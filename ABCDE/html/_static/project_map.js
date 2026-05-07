7981049162024621cad31698e1c67a0f82f3e583
polarion <polarion@b5bd34d7e592>
2026-05-07 12:59:23 +0000

    ABCDEF

---
+++ ABCDE/html/_static/project_map.js
@@ -0,0 +1,51 @@
+// map of the project for the stable_uri forwarder
+const projectMap = {
+ "input/0.html": [
+
+
+  {"_LINK":"None-Catalog-Specification" },
+
+  {"UID":"EL-183","_LINK":"EL-183" },
+
+  {"UID":"EL-184","_LINK":"EL-184" },
+
+  {"_LINK":"e23bea75948d4862bb0d5243b086d149" },
+
+  {"UID":"EL-185","_LINK":"EL-185" },
+
+  {"_LINK":"7926d1c7c9644745a575142cd6c9b90e" },
+
+  {"UID":"EL-144","_LINK":"EL-144" },
+
+  {"UID":"EL-145","_LINK":"EL-145" },
+
+  {"UID":"EL-147","_LINK":"EL-147" },
+
+  {"UID":"EL-149","_LINK":"EL-149" },
+
+  {"UID":"EL-148","_LINK":"EL-148" },
+
+  {"UID":"EL-150","_LINK":"EL-150" },
+
+  {"UID":"EL-101","_LINK":"EL-101" },
+
+  {"UID":"EL-146","_LINK":"EL-146" },
+
+  {"UID":"EL-152","_LINK":"EL-152" },
+
+  {"UID":"EL-151","_LINK":"EL-151" },
+
+  {"UID":"EL-153","_LINK":"EL-153" },
+
+  {"UID":"EL-156","_LINK":"EL-156" },
+
+  {"UID":"EL-157","_LINK":"EL-157" },
+
+  {"UID":"EL-158","_LINK":"EL-158" },
+
+  {"UID":"EL-159","_LINK":"EL-159" },
+
+  {"UID":"EL-155","_LINK":"EL-155" },
+
+  {"UID":"EL-160","_LINK":"EL-160" }, ],
+};
