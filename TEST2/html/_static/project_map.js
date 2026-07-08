d236c15246376df06bd9f82ad93b7faecb448342
StrictDoc Exporter <polarion-opensource@sbb.ch>
2026-07-08 11:43:23 +0000

    TEST2

---
+++ TEST2/html/_static/project_map.js
@@ -0,0 +1,29 @@
+// map of the project for the stable_uri forwarder
+window.StrictDoc = window.StrictDoc || {};
+window.StrictDoc.project = window.StrictDoc.project || {};
+window.StrictDoc.project.map = {
+ "input/0.html": [
+
+
+  {"_LINK":"None-Administration-Specification" },
+
+  {"UID":"EL-161","_LINK":"EL-161" },
+
+  {"UID":"EL-187","_LINK":"EL-187" },
+
+  {"_LINK":"3523a0a2f69647dabdd0cd4bb905de19" },
+
+  {"UID":"EL-186","_LINK":"EL-186" },
+
+  {"_LINK":"17b4d45a322d435a915370808d7b9568" },
+
+  {"UID":"EL-162","_LINK":"EL-162" },
+
+  {"UID":"EL-163","_LINK":"EL-163" },
+
+  {"UID":"EL-165","_LINK":"EL-165" },
+
+  {"UID":"EL-166","_LINK":"EL-166" },
+
+  {"UID":"EL-164","_LINK":"EL-164" }, ],
+};
